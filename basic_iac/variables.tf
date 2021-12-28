862178c40cb1aca3183ff41b5285d6a56f4295f8
Durgesh Nayak <durgesh.nayak@outlook.com>
2021-12-28 17:11:48 +0530

    Added project

---
+++ basic_iac/variables.tf
@@ -0,0 +1,10 @@
+#AWS Credentials
+variable "aws_access_key" {
+    type = string
+    description = "AWS Access Key ID"
+}
+
+variable "aws_secret_key" {
+  type = string
+  description = "AWS Secret Key"
+}
