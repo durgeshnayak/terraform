c6d68b1213544862de9829db74bde483e03e70fd
Durgesh Nayak <durgesh.nayak@outlook.com>
2021-12-30 12:36:15 +0530

    added ec2 instance to public subnet

--- basic_iac/resources.tf
+++ basic_iac/resources.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0a23ccb2cdd9286bb"
   instance_type = "t2.micro"
 
+  subnet_id = aws_subnet.publicsubnet_1a.id
+
   tags = {
     Name = var.instance_name
   }
